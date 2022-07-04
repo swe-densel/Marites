fa8e6149e0ac013d2f8e74d86bd5ad2070d1a3bc
swe-densel <denselross.serrano@gcash.com>
2022-07-04 17:18:36 +0800

    - Layout for Contacts DONE - Layout for Chat IN PROGRESS

--- app/src/main/java/ph/gcash/marites/ContactsActivity.kt
+++ app/src/main/java/ph/gcash/marites/ContactsActivity.kt
@@ -3,6 +3,9 @@ package ph.gcash.marites
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
 import ph.gcash.marites.databinding.ActivityContactsBinding
 import ph.gcash.marites.databinding.ActivityLoginBinding
 
@@ -19,4 +22,25 @@ class ContactsActivity : AppCompatActivity() {
         val email = intent.getStringExtra("email")
         Log.d(logger, "Email: $email")
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_actionbar, menu)
+        val searchItem = menu?.findItem(R.id.action_search)
+
+        val searchView = searchItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
 }
+
+
